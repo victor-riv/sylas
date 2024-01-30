98a6ee9a0344520a3a2c85ef97613fd8d6a7b366
Victor Rivera <verm2405@gmail.com>
2024-01-29 21:38:27 -0500

    Initial Commit

---
+++ sylas_swiftui/sylas_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sylas_swiftuiApp.swift
+//  sylas_swiftui
+//
+//  Created by Victor Rivera on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct sylas_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
