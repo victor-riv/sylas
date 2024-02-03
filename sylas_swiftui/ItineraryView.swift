c3a7f74e473723eef84f0ad2a73ee5d5d7378f0b
Victor Rivera <verm2405@gmail.com>
2024-02-03 11:21:07 -0500

    Add Location button

--- sylas_swiftui/ItineraryView.swift
+++ sylas_swiftui/ItineraryView.swift
@@ -13,68 +13,84 @@ struct ItineraryView: View {
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                VStack(alignment: .leading) {
-                    DestinationCardView()
-                    HStack (alignment: .bottom){
-                        VStack (alignment: .leading,spacing: 12){
-                            HStack (spacing: 12) {
-                                VStack (alignment: .leading) {
-                                    Text("Itinerary for Madrid")
-                                        
-                                    Text("from Feb 2-5, 2024").font(.footnote)
-                                }
-                            }
-                            HStack {
-                                ForEach(1...4, id: \.self) { day in
-                                    Button(action: {
-                                        self.toggledID = day
-                                    }) {
-                                        
-                                        Text("D\(day)")
-                                            .padding(5)
-                                            .frame(width: 40, height: 35)
-                                            .background(day == toggledID ? Color(red: 224 / 255, green: 227 / 255, blue: 72 / 255) : Color(red: 107/255, green: 107/255, blue: 107/255))
-                                            .foregroundColor(Color.black)
-                                            .cornerRadius(10)
+            VStack {
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        DestinationCardView()
+                        HStack (alignment: .bottom){
+                            VStack (alignment: .leading,spacing: 12){
+                                HStack (spacing: 12) {
+                                    VStack (alignment: .leading) {
+                                        Text("Itinerary for Madrid")
+                                            
+                                        Text("from Feb 2-5, 2024").font(.footnote)
                                     }
                                 }
-                                Button(action: {}, label: {
-                                    Image(systemName: "plus")
-                                        .padding(5).frame(width: 40, height: 35)
-                                    
-                                        .background(Color(red: 107/255, green: 107/255, blue: 107/255))
-                                        .foregroundColor(Color.black)
-                                        .cornerRadius(10)
-                                })
+                                HStack {
+                                    ForEach(1...4, id: \.self) { day in
+                                        Button(action: {
+                                            self.toggledID = day
+                                        }) {
+                                            
+                                            Text("D\(day)")
+                                                .padding(5)
+                                                .frame(width: 40, height: 35)
+                                                .background(day == toggledID ? Color(red: 224 / 255, green: 227 / 255, blue: 72 / 255) : Color(red: 107/255, green: 107/255, blue: 107/255))
+                                                .foregroundColor(Color.black)
+                                                .cornerRadius(10)
+                                        }
+                                    }
+                                    Button(action: {}, label: {
+                                        Image(systemName: "plus")
+                                            .padding(5).frame(width: 40, height: 35)
+                                        
+                                            .background(Color(red: 107/255, green: 107/255, blue: 107/255))
+                                            .foregroundColor(Color.black)
+                                            .cornerRadius(10)
+                                    })
+                                }
+                                
                             }
+                            Spacer()
+                            Image("map")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 85, height: 85)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .stroke(Color(red: 52/255.0, green: 51/255.0, blue: 50/255.0))
+                                )
+                        }
+                        
+                        .padding(.bottom)
+                        VStack (spacing: 2){
+                            LocationTile(locationName: "Parque de El Retiro", locationAddress: "Retiro, 28009 Madrid, Spain")
+                            
+                            LocationTile(locationName: "Parque de El Retiro", locationAddress: "Retiro, 28009 Madrid, Spain")
+                            
                             
                         }
-                        Spacer()
-                        Image("map")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 85, height: 85)
-                            .clipShape(RoundedRectangle(cornerRadius: 20))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 20)
-                                    .stroke(Color(red: 52/255.0, green: 51/255.0, blue: 50/255.0))
-                            )
-                    }
-                    
-                    .padding(.bottom)
-                    VStack (spacing: 2){
-                        LocationTile(locationName: "Parque de El Retiro", locationAddress: "Retiro, 28009 Madrid, Spain")
-                        
-                        LocationTile(locationName: "Parque de El Retiro", locationAddress: "Retiro, 28009 Madrid, Spain")
-                        
-                        Button(action: authenticator.signOut) {
-                            Text("Sign Out")
-                        }
                         
                     }
-                    
                 }
+                Spacer()
+                Button(action: authenticator.signOut) {
+                    Text("Add Location")
+                        .frame(maxWidth: .infinity) // Ensures the button stretches to the full width
+                        .padding() // Provides padding inside the button
+                        .foregroundColor(Color.white)
+                        .background(Color.black) // Button's background color
+                        .cornerRadius(10) // Rounds the corners of the button
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12) // Matches the cornerRadius of the button
+                                .stroke(Color.white, lineWidth: 1) // Apply stroke border with color and line width
+                        )
+                }
+                .padding(.horizontal) // Apply padding here to respect the view's padding
+                .padding(.bottom)
+
+                
             }
             Spacer()
         }
