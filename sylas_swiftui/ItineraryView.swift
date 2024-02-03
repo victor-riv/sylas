515895fbd846f461896285983af3a8606970e178
Victor Rivera <verm2405@gmail.com>
2024-02-03 16:18:08 -0500

    Make Itinerary Buttons section sticky

--- sylas_swiftui/ItineraryView.swift
+++ sylas_swiftui/ItineraryView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ItineraryView: View {
     @State private var toggledID: Int = 1
+    @State private var isSticky: Bool = false
     @EnvironmentObject var authenticator: Authenticator
     
     var body: some View {
@@ -17,54 +18,17 @@ struct ItineraryView: View {
                 ScrollView {
                     VStack(alignment: .leading) {
                         DestinationCardView()
-                        HStack (alignment: .bottom){
-                            VStack (alignment: .leading,spacing: 12){
-                                HStack (spacing: 12) {
-                                    VStack (alignment: .leading) {
-                                        Text("Itinerary for Madrid")
-                                            
-                                        Text("from Feb 2-5, 2024").font(.footnote)
-                                    }
-                                }
-                                HStack {
-                                    ForEach(1...4, id: \.self) { day in
-                                        Button(action: {
-                                            self.toggledID = day
-                                        }) {
-                                            
-                                            Text("D\(day)")
-                                                .padding(5)
-                                                .frame(width: 40, height: 35)
-                                                .background(day == toggledID ? Color(red: 224 / 255, green: 227 / 255, blue: 72 / 255) : Color(red: 107/255, green: 107/255, blue: 107/255))
-                                                .foregroundColor(Color.black)
-                                                .cornerRadius(10)
-                                        }
-                                    }
-                                    Button(action: {}, label: {
-                                        Image(systemName: "plus")
-                                            .padding(5).frame(width: 40, height: 35)
-                                        
-                                            .background(Color(red: 107/255, green: 107/255, blue: 107/255))
-                                            .foregroundColor(Color.black)
-                                            .cornerRadius(10)
-                                    })
-                                }
-                                
-                            }
-                            Spacer()
-                            Image("map")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 85, height: 85)
-                                .clipShape(RoundedRectangle(cornerRadius: 20))
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 20)
-                                        .stroke(Color(red: 52/255.0, green: 51/255.0, blue: 50/255.0))
-                                )
-                        }
-                        
-                        .padding(.bottom)
+                        stickiedContent
+                            .mySticky(maxHeight: 400)
                         VStack (spacing: 2){
+                            LocationTile(locationName: "Parque de El Retiro", locationAddress: "Retiro, 28009 Madrid, Spain")
+                            
+                            LocationTile(locationName: "Parque de El Retiro", locationAddress: "Retiro, 28009 Madrid, Spain")
+                            
+                            LocationTile(locationName: "Parque de El Retiro", locationAddress: "Retiro, 28009 Madrid, Spain")
+                            
+                            LocationTile(locationName: "Parque de El Retiro", locationAddress: "Retiro, 28009 Madrid, Spain")
+                            
                             LocationTile(locationName: "Parque de El Retiro", locationAddress: "Retiro, 28009 Madrid, Spain")
                             
                             LocationTile(locationName: "Parque de El Retiro", locationAddress: "Retiro, 28009 Madrid, Spain")
@@ -74,27 +38,122 @@ struct ItineraryView: View {
                         
                     }
                 }
+                .coordinateSpace(name: "itineraryContainer")
+                
                 Spacer()
                 Button(action: authenticator.signOut) {
                     Text("Add Location")
-                        .frame(maxWidth: .infinity) // Ensures the button stretches to the full width
-                        .padding() // Provides padding inside the button
+                        .frame(maxWidth: .infinity)
+                        .padding()
                         .foregroundColor(Color.white)
-                        .background(Color.black) // Button's background color
-                        .cornerRadius(10) // Rounds the corners of the button
+                        .background(Color.black)
+                        .cornerRadius(12)
                         .overlay(
-                            RoundedRectangle(cornerRadius: 12) // Matches the cornerRadius of the button
-                                .stroke(Color.white, lineWidth: 1) // Apply stroke border with color and line width
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(Color.white, lineWidth: 1)
                         )
                 }
-                .padding(.horizontal) // Apply padding here to respect the view's padding
+                .padding(.horizontal)
                 .padding(.bottom)
-
+                
                 
             }
             Spacer()
         }
         .padding(.horizontal, 5)
+    }
+    
+    @ViewBuilder var stickiedContent: some View {
+        HStack (alignment: .bottom){
+            VStack (alignment: .leading,spacing: 12){
+                HStack (spacing: 12) {
+                    VStack (alignment: .leading) {
+                        Text("Itinerary for Madrid")
+                        
+                        Text("from Feb 2-5, 2024").font(.footnote)
+                    }
+                }
+                HStack {
+                    ForEach(1...4, id: \.self) { day in
+                        Button(action: {
+                            self.toggledID = day
+                        }) {
+                            
+                            Text("D\(day)")
+                                .padding(5)
+                                .frame(width: 40, height: 35)
+                                .background(day == toggledID ? Color(red: 224 / 255, green: 227 / 255, blue: 72 / 255) : Color(red: 107/255, green: 107/255, blue: 107/255))
+                                .foregroundColor(Color.black)
+                                .cornerRadius(10)
+                        }
+                    }
+                    Button(action: {}, label: {
+                        Image(systemName: "plus")
+                            .padding(5).frame(width: 40, height: 35)
+                        
+                            .background(Color(red: 107/255, green: 107/255, blue: 107/255))
+                            .foregroundColor(Color.black)
+                            .cornerRadius(10)
+                    })
+                }
+                
+            }
+            Spacer()
+            Image("map")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 85, height: 85)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color(red: 52/255.0, green: 51/255.0, blue: 50/255.0))
+                )
+        }
+        .padding(.bottom)
+    }
+}
+
+struct MySticky: ViewModifier {
+    @State var frame: CGRect = .zero
+    var maxHeight: CGFloat
+    
+    var isSticking: Bool {
+        frame.minY < 0
+    }
+    func body(content: Content) -> some View {
+        content
+            .background(isSticking ? Color.systemBackground : Color.clear)
+            .offset(y: isSticking ? -frame.minY : 0)
+            .zIndex(isSticking ? 1 : 0)
+            .background(
+                GeometryReader { proxy in
+                    if isSticking {
+                        Color.systemBackground
+                            .frame(height: maxHeight + abs(frame.minY))
+                            .offset(y: -maxHeight)
+                    } else {
+                        Color.clear
+                    }
+                }
+            )
+            .overlay(GeometryReader { proxy in
+                let f = proxy.frame(in: .named("itineraryContainer"))
+                Color.clear.onAppear{ frame = f}
+                    .onChange(of: f) { frame = $0 }
+            })
+    }
+}
+
+extension View {
+    func mySticky(maxHeight: CGFloat) -> some View {
+        modifier(MySticky(maxHeight: maxHeight))
+    }
+}
+
+extension Color {
+    static var systemBackground: Color {
+        // Use different colors for light and dark mode if needed
+        Color(UIColor.systemBackground)
     }
 }
 
